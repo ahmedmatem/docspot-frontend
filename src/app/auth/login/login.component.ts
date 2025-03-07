dc20a56a08dab79dbfd54961d4eb7fce85a5bcc5
Ahmed Matem <ahmedmatem@gmail.com>
2025-03-07 12:20:41 +0200

    CReate template driven login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
 
+  handleSubmit(form: any){
+    console.log(form.value.username);
+  }
 }
